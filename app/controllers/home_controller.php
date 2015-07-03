fc75bca16bb0fda037f5c87245533f89059f2480
Sven Marc Gehring <svengehring@cybrox.eu>
2015-07-03 10:30:09 +0200

    Request user data on initial load

--- app/controllers/home_controller.php
+++ app/controllers/home_controller.php
@@ -3,7 +3,31 @@
     class HomeController extends BaseController {
 
         public function login() {
-            View::partial('login');
+            View::render('login');
+        }
+
+
+        public function fox() {
+            $c = curl_init('http://www.steamgifts.com/giveaways/search?type=wishlist');
+
+            curl_setopt($c, CURLOPT_VERBOSE, TRUE);
+            curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($c, CURLOPT_COOKIE, 'PHPSESSID='.Session::get('phps'));
+
+            session_write_close();
+            $page = curl_exec ($c);
+
+            curl_close ($c);
+            session_start();
+
+            if (strlen($page) == 0) {
+                Router::redirect('./key');
+            }
+        }
+
+
+        public function key() {
+            View::render('key');
         }
 
     }
