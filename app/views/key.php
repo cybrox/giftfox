25e20ed031c1c864920d97182adfc1137e6f6fd8
Sven Gehring <marc@cybrox.eu>
2015-07-04 15:44:31 +0200

    Better logout button for key page

--- app/views/key.php
+++ app/views/key.php
@@ -47,12 +47,12 @@
 </script>
 
 <div class="key-form">
-    <h3 class="info-message"><a>steamgifts.com</a> session has expired! Copy new session key.</h3>
+    <h3 class="info-message">
+        <a>steamgifts.com</a> session has expired!
+        Copy new session key
+        (<a data-query="click(logout)">Logout</a>)
+    </h3>
     <input class="key-input" type="text" placeholder="sessionkey" data-query="val(sesskey).keyup(savekey)" />
     <p class="form-error form-error1 hidden">Something failed!</p>
     <p class="form-error form-error2 hidden">Key too short!</p>
-
-    <div class="fox-footer">
-        <a class="fox-logout" data-query="click(logout)">Logout</a>
-    </div>
 </div>
