c8987631d9e8810a52126b13a0e1a2a9d15fc00d
Sven Gehring <marc@cybrox.eu>
2015-07-15 21:49:23 +0200

    Reformat year on overview page

--- app/views/fox.php
+++ app/views/fox.php
@@ -85,7 +85,7 @@
         <div class="row">
             <div class="col-xs-6"><span class="fox-title">Last Auto-Join</span></div>
             <div class="col-xs-6"><span class="fox-title">
-                <?php echo date("d.m H:i:s", Registry::get('lastjoin')); ?>
+                <?php echo date("d.m.Y - H:i", Registry::get('lastjoin')); ?>
             </span></div>
         </div>
         <div class="row justify fox-waiting">
