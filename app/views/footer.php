d99cf810a86975e027b5e1ebabd8aa90cfadcc26
Sven Gehring <marc@cybrox.eu>
2015-07-16 17:33:02 +0200

    Linked footer copyright

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,7 +1,7 @@
     </section>
 
     <footer class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
-        <span class="page-info">Built with &hearts; by <a>cybrox</a></span>
+        <span class="page-info">Built with &hearts; by <a href="http://cybrox.eu">cybrox</a></span>
     </footer>
 
 </body>
